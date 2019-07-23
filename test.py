5d9fe46606cb17428f9e08ba3e746143d803577a
Anthony Johnson <anthony.joseph.johnson@gmail.com>
2019-07-23 16:38:53 -0500

    testing PSA connectivity

--- test.py
+++ test.py
@@ -1,65 +1,56 @@
 import requests
 from getpass import getpass
 
-
 headers = {"content-type": "application/json"}
 
 # Variables
 pAuth = "/api/v1/auth/"
 pUserRoles = "/api/v1/configuration/users/user-roles/"
 
-# Tests
-# input("Hostname : ")
-url = "10.100.100.251"
-# input("Username : ")
-uname = "admin"
-# getpass("Password : ")
-passwd = "Juniper12345!!"
+host = input("Hostname : ")
+uname = input("Username : ")
+passwd = getpass("Password : ")
 
+# Connect with username/password to retreive api_key
+r = requests.get(
+    "https://" + host + pAuth, verify=False, auth=(uname, passwd), headers=headers
+)
+
+# Show feedback
+print("*" * 80)
+auth_title = "Auth Information"
+print(f"{auth_title:^80}")
+print(f"Headers:          {headers}")
+print(f"Path to Auth:     {pAuth}")
+print(f"Host:             {host}")
+print(f"Username:         {uname}")
+print(f"Password:         {passwd}")
+print()
+print(f"Status Code:      {r.status_code}")
+print(f"JSON Information: {r.json()}")
+d = r.json()
+print(f"JSON Key:         {d['api_key']}")
+api_key = d["api_key"]
+print("*" * 80)
+
+# Connect with api_key and pull configuration information
 try:
-    r = requests.get(
-        "https://" + url + pAuth, verify=False, auth=(uname, passwd), headers=headers
+    config_request = requests.get(
+        "https://10.100.100.251/api/v1/configuration", verify=False, auth=(api_key, "")
     )
 except requests.exceptions.ConnectionError:
     r.status_code = "Connection Refused"
 
-
+# Show feedback
 print("*" * 80)
-print(f"Auth Information")
-print()
-print("--- Status Code ---")
-print(r.status_code)
-print("--- text ---")
-print(r.text)
-print("--- Headers ---")
-print(r.headers)
-print("--- JSON response ---")
-print(r.json())
-d = r.json()
-print("--- d ---")
-api_key = d["api_key"]
-print(api_key)
-print(r.content)
-print("*" * 80)
-
-"""
-try:
-    config_request = requests.get('https://10.100.100.251/api/v1/configuration', verify=False, auth=(api_key, ''))
-except requests.exceptions.ConnectionError:
-    r.status_code = "Connection Refused"
-
-print("*" * 80)
-print("Config Information")
-print()
-print(config_request.content)
+config_title = "Configuration Information"
+print(f"{config_title:^80}")
 print()
 data = config_request.json()
 print(data)
 for key in data:
-    print(key)
+    print(f"{key} equal to {data[key]}")
     data1 = data[key]
     for element in data1:
-        print(element)
+        print(f"{element} equal to {data1[element]}")
 print("*" * 80)
-"""
-
